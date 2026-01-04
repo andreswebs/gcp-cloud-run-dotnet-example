b3acf799be631a7fb745ecd1afd812f01e65bae0
Andre Silva <andreswebs@pm.me>
2026-01-04 18:54:29 -0300

    datadog wip
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- terraform/envs/sandbox/main.tf
+++ terraform/envs/sandbox/main.tf
@@ -10,32 +10,53 @@ module "service" {
 
   containers = [
     {
-      name  = "example-api"
-      image = var.image_uri
+      name        = "example-api"
+      image       = var.image_uri
+      description = "An example .NET web api to test Datadog configuration"
+      resources = {
+        limits = {
+          cpu    = "1"
+          memory = "512Mi"
+        }
+      }
       liveness_probe = {
         http_get = {
           path = "/healthz"
         }
       }
       env = [
-        # {
-        #   name  = "TEST"
-        #   value = "ok"
-        # },
         {
-          name = "TEST"
-          value_source = {
-            secret_key_ref = {
-              secret = "TEST"
-            }
-          }
+          name  = "TEST"
+          value = "ok"
         },
       ]
     },
+    # {
+    #   name        = "datadog-sidecar"
+    #   image       = "gcr.io/datadoghq/serverless-init:latest"
+    #   description = "Datadog sidecar"
+    #   resources = {
+    #     limits = {
+    #       cpu    = "1"
+    #       memory = "512Mi"
+    #     }
+    #   }
+    #   env = [
+    #     {
+    #       name = "DD_API_KEY"
+    #       value_source = {
+    #         secret_key_ref = {
+    #           secret = "DD_API_KEY"
+    #         }
+    #       }
+    #     },
+    #   ]
+    #   health_port = 5555 # DD_HEALTH_PORT
+    # },
   ]
 
   secrets_access = [
-    "TEST",
+    "DD_API_KEY",
   ]
 
 }
